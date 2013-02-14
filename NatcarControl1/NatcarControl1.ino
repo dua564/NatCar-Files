230d1e394de729389b5c64f8a2d4358384d286e4
Nikhil Dua <nikhil9dua@gmail.com>
2013-02-14 13:11:30 -0800

    Commit 2

---
+++ NatcarControl1/NatcarControl1.ino
@@ -0,0 +1,111 @@
+                                                                                 
+// UCLA NATCAR PROJECT - TEAM DIZHEL ///// ------> VERSION 0.1
+
+// Version Update Information
+// 0.1 Set pins, initialize paramaters, line cam acquisition 2/17/13
+
+
+
+
+// Line Camera
+#define AOpin A0  // Collect data from Linecam
+#define CLKpin 2  // Ctrl Linecam CLOCK
+#define SIpin 3   // Ctrl Linecam
+
+#define NPIXELS 128
+
+// Motor Controller 
+#define MOTORpin 5  // output PWN to ctrl speed
+#define STOPpin 6    // output to ctrl fast stop switch
+
+// Servo
+#include <Servo.h> 
+#define SERVOpin 8   //output to control servo 
+Servo natServo;
+
+const int straight = 71;
+const int hardLeft = 98;
+const int hardRight = 44;
+  // turn range 44 - 98 (right to left)
+  // straight 71
+
+///////////////////////////////////////////////////////////////////////
+byte Pixel[NPIXELS];
+
+void setup (void)
+{
+  //SETPUT PINS
+       //pinMode (AOpin, INPUT);
+       pinMode (SIpin, OUTPUT);
+       pinMode (CLKpin, OUTPUT);
+       
+       pinMode (MOTORpin, OUTPUT);
+       pinMode (STOPpin, OUTPUT);
+    
+       natServo.attach(SERVOpin);  // attaches the servo pin to the servo object, basically pinMode
+   
+   //Initialize Camera Paramters
+       digitalWrite (SIpin, LOW);
+       digitalWrite (CLKpin, HIGH);
+
+       Serial.begin (115200);  
+}
+
+int i;
+int maxi = 0;
+int motorPWM = 0;
+int motorSpeed = 0;
+int turnLeft = 71;
+int turnRight = 71;
+/////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+void loop (void)
+{
+   maxi = 0;
+   
+   ///// ACQUIRE DATA FROM LINECAM ////////////////////////////////////////
+           // SI pulse to capture the image
+           digitalWrite (CLKpin, LOW);
+           delayMicroseconds (1);
+           digitalWrite (SIpin, HIGH);    
+           digitalWrite (CLKpin, HIGH);
+           digitalWrite (SIpin, LOW);
+         
+           // pulse CLK and acquire image into Arduino && Look for CENTER pixel
+           for (i = 0; i < NPIXELS; i++) {
+             Pixel[i] = analogRead (AOpin) / 4;  // analogRead 0 - 1053 && Pixel is byte type 0 - 255
+             digitalWrite (CLKpin, LOW);        
+             delayMicroseconds (1);
+             
+             if (Pixel[i] > maxpx) 
+             {
+                 maxi = i;
+             }
+             digitalWrite (CLKpin, HIGH);
+           }
+           
+           delayMicroseconds (1);
+             
+    ///// CONTROL STEERING //////////////////////////////////////////   
+           if (maxi < 61)
+           {
+                 turnLeft = map(maxi, 0, 59, 44, 70);
+                 natServo.write(turnLeft);
+           }
+           else if (maxi > 67)  
+           {
+                 turnRight = map(maxi, 68, 127, 72, 98); 
+                 natServo.write(turnRight);
+           }
+           else
+           {
+               natServo.write(straight);
+           }
+          
+    ///// CONTROL MOTOR SPEED/////////////////////////////////////////
+           motorPWM = 10;
+           motorSpeed = map(motorPWM, 0, 100, 0, 255);
+           
+           analogWrite(MOTORpin, motorSpeed);
+
+}
---
+++ ServoControlTest1/ServoControlTest1.ino
@@ -0,0 +1,41 @@
+// Sweep
+// by BARRAGAN <http://barraganstudio.com> 
+// This example code is in the public domain.
+
+
+#include <Servo.h> 
+ 
+Servo myservo;  // create servo object to control a servo 
+                // a maximum of eight servo objects can be created 
+ 
+int pos = 0;    // variable to store the servo position 
+int turnstart = 35;
+int turnend = 105;
+
+
+void setup() 
+{ 
+  myservo.attach(9);  // attaches the servo on pin 9 to the servo object
+  Serial.begin(9600); 
+} 
+ 
+ 
+void loop() 
+{ 
+  for(pos = turnstart; pos < turnend; pos += 1)  // goes from 0 degrees to 180 degrees 
+  {                                  // in steps of 1 degree 
+    myservo.write(pos);              // tell servo to go to position in variable 'pos'
+    Serial.println(pos); 
+    delay(100);                       // waits 15ms for the servo to reach the position 
+  } 
+  
+  for(pos = turnend; pos > turnstart; pos-=1)     // goes from 180 degrees to 0 degrees 
+  {                                
+    myservo.write(pos);              // tell servo to go to position in variable 'pos'
+    Serial.println(pos); 
+    delay(100);                       // waits 15ms for the servo to reach the position 
+  } 
+
+//  myservo.write(69);
+
+} 

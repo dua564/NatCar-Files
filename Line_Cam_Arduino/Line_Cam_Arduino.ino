06763b82a2b8966b62bf088ea9ebc9390b322cdd
Jennifer Lopez <jeffreyh192@gmail.com>
2013-04-15 22:28:04 -0700

    new April 13

--- Line_Cam_Arduino/Line_Cam_Arduino.ino
+++ Line_Cam_Arduino/Line_Cam_Arduino.ino
@@ -1,13 +1,13 @@
 #define AOpin A0  // Analog pin 0
-#define CLKpin 2
-#define SIpin 4
+#define CLKpin 3
+#define SIpin 5
 
 #define NPIXELS 128
 
 byte Pixel[NPIXELS];
-int ch = 0;
-int maxi;
-int maxpx = 0;
+//int ch = 0;
+//int maxi;
+//int maxpx = 0;
 
 
 void setup (void)
@@ -22,13 +22,10 @@ void setup (void)
    Serial.begin (115200);
 }
 
+int i;
 
 void loop (void)
 {
-   int i;
-   byte ch;
-   byte maxpx;
-   int maxi = 0;
 
    // SI pulse to capture the image
    digitalWrite (CLKpin, LOW);
@@ -39,39 +36,27 @@ void loop (void)
 
    // pulse CLK and acquire image into Arduino
    for (i = 0; i < NPIXELS; i++) {
+//     Serial.print(analogRead (AOpin) / 4);
+//     Serial.print(", ");
+     
      Pixel[i] = analogRead (AOpin) / 4;// analogRead 0 - 1053 && Pixel is byte type 0 - 255
      digitalWrite (CLKpin, LOW);
      delayMicroseconds (1);
      digitalWrite (CLKpin, HIGH);
    }
-
    delayMicroseconds (1);
 
-////////////////////////////////////////////////////////////////////////////////
-
-   for (i = 0; i < NPIXELS; i++) 
-   {
-       ch = Pixel[i];
-       if (ch < 0)
-         ch += 256;
-       
-       if (ch > maxpx) {   // Look for brightest pixel
-         maxi = i;
-         maxpx = Pixel[i];
-         Serial.println(maxi);
-       }
-   }
-   
-  // Serial.println(maxi);
-
-   delay(100);
-
-//   Serial.write ((byte)0);  // cast 0 to byte type variable, use as STARTING PT
-//   
+//   Serial.println("");
 //   for (i = 0; i < NPIXELS; i++) {
-//     if (Pixel[i] == 0)
-//       Serial.write ((byte)1);  // change 0s to 1s so not confused w/ STARTING PT
-//     else
-//       Serial.write ((byte)Pixel[i]);  // writes bytes to serial port
+//     println(Pixel[i]);
+//     println("NEXTSECTIONOFDATA");
 //   }
+
+   Serial.write ((byte)0);  // cast 0 to byte type variable, use as STARTING PT 
+   for (i = 0; i < NPIXELS; i++) {
+     if (Pixel[i] == 0)
+       Serial.write ((byte)1);  // change 0s to 1s so not confused w/ STARTING PT
+     else
+       Serial.write ((byte)Pixel[i]);  // writes bytes to serial port
+   }
 }

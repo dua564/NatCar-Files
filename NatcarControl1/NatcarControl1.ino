88fbcc2dc009a746f16dee759e35f7158b1512bc
Jennifer Lopez <jeffreyh192@gmail.com>
2013-04-15 22:28:46 -0700

    new April 13

--- NatcarControl1/NatcarControl1.ino
+++ NatcarControl1/NatcarControl1.ino
@@ -1,16 +1,13 @@
-                                                                                 
-// UCLA NATCAR PROJECT - TEAM DIZHEL ///// ------> VERSION 0.2
+// UCLA NATCAR PROJECT - TEAM DIZHEL ///// ------> VERSION 0.2 ALPHA
 // Version Update Information
-// 0.1 Set pins, initialize paramaters, line cam acquisition 2/17/13
-// 0.2 implement new camera data acquisition algorithm
+// 0.1 Set pins, initialize paramaters, line cam acquisition 2/7/13
+// 0.2 implement new camera data acquisition algorithm, brightest pixel 2/15/13
 
 
 // Line Camera
 #define AOpin A0  // Collect data from Linecam
-#define CLKpin 2  // Ctrl Linecam CLOCK
-#define SIpin 4   // Ctrl Linecam
-
-#define NPIXELS 128
+#define CLKpin 3  // Ctrl Linecam CLOCK
+#define SIpin 5   // Ctrl Linecam
 
 // Motor Controller 
 #define MOTORpin 6  // output PWN to ctrl speed
@@ -18,103 +15,122 @@
 
 // Servo
 #include <Servo.h> 
-#define SERVOpin 9   //output to control servo 
+#define SERVOpin 10   //output to control servo 
 Servo natServo;
 
+////////////////////////////////////////////////////////////////////////
+const int NPIXELS = 128;  
 const int straight = 71;
 const int hardLeft = 98;
 const int hardRight = 44;
-  // turn range 44 - 98 (right to left)
-  // straight 71
+const int memorySize = 10;
+// turn range 44 - 98 (right to left)
+// straight 71
 
 ///////////////////////////////////////////////////////////////////////
-byte Pixel[NPIXELS];
-
+byte Pixel[NPIXELS];    // storage var for incoming data from cam
+int history[memorySize] = {straight}; // store all 71s in history initially
+                                    // assume car on track in beginning
+                                    
 void setup (void)
 {
   //SETPUT PINS
-       //pinMode (AOpin, INPUT);
-       pinMode (SIpin, OUTPUT);
-       pinMode (CLKpin, OUTPUT);
-       
-       pinMode (MOTORpin, OUTPUT);
-       pinMode (STOPpin, OUTPUT);
-    
-       natServo.attach(SERVOpin);  // attaches the servo pin to the servo object, basically pinMode
-       natServo.write(71);
-   //Initialize Camera Paramters
-       digitalWrite (SIpin, LOW);
-       digitalWrite (CLKpin, HIGH);
+  //pinMode (AOpin, INPUT);
+  pinMode (SIpin, OUTPUT);
+  pinMode (CLKpin, OUTPUT);
 
-       Serial.begin (115200); 
+  pinMode (MOTORpin, OUTPUT);
+  pinMode (STOPpin, OUTPUT);
+
+  natServo.attach(SERVOpin);  // attaches the servo pin to the servo object, basically pinMode
+  natServo.write(71);
+  
+  //Initialize Camera Paramters
+  digitalWrite (SIpin, LOW);
+  digitalWrite (CLKpin, HIGH);
+
+  Serial.begin (115200); 
 }
 
-
-      
 int i;
 byte ch;
 byte maxpx;
 int maxi = 0;
 int motorPWM = 0;
 int motorSpeed = 0;
-int turnLeft = 71;
-int turnRight = 71;
+int turnAngle = 71;
+
 
 /////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 
 void loop (void)
 {
-         maxpx = 0;
-        // SI pulse to capture the image
-         digitalWrite (CLKpin, LOW);
-         delayMicroseconds (1);
-         digitalWrite (SIpin, HIGH);
-         digitalWrite (CLKpin, HIGH);
-         digitalWrite (SIpin, LOW);
-      
-         // pulse CLK and acquire image into Arduino
-         for (i = 0; i < NPIXELS; i++) {
-           Pixel[i] = analogRead (AOpin) / 4;// analogRead 0 - 1053 && Pixel is byte type 0 - 255
-           digitalWrite (CLKpin, LOW);
-           delayMicroseconds (1);
-           digitalWrite (CLKpin, HIGH);
-         }
-      
-         delayMicroseconds (1);
-      
-         for (i = 0; i < NPIXELS; i++) 
-         {
-             ch = Pixel[i];
-             if (ch < 0)
-               ch += 256;
-             
-             if (ch > maxpx) {   // Look for brightest pixel
-               maxi = i;
-               maxpx = Pixel[i];
-             }
-         }
-       
+    ///// LINESCAN CAM DATA ACQUISITION //////////////////////////////////////////   
+          maxpx = (byte)0;  // reset maxpx to 0 every time
           
-    ///// CONTROL STEERING //////////////////////////////////////////   
-           if (maxi > 67)
-           {
-                 turnLeft = map(maxi, 0, 59, 44, 70);
-                 natServo.write(turnLeft);
-           }
-           else if (maxi < 61)  
-           {
-                 turnRight = map(maxi, 68, 127, 72, 98); 
-                 natServo.write(turnRight);
-           }
+          // pulse SI to capture the image
+          digitalWrite (CLKpin, LOW);
+          delayMicroseconds (1);
+          digitalWrite (SIpin, HIGH);
+          digitalWrite (CLKpin, HIGH);
+          digitalWrite (SIpin, LOW);
+  
+          // pulse CLK and acquire image into Arduino
+          for (i = 0; i < NPIXELS; i++) {
+            Pixel[i] = analogRead (AOpin) / 4;// analogRead 0 - 1053 && Pixel is byte type 0 - 255
+            digitalWrite (CLKpin, LOW);
+            delayMicroseconds (1);
+            digitalWrite (CLKpin, HIGH);
+          }
+          delayMicroseconds (1);
+        
+       Serial.write ((byte)0);  // cast 0 to byte type variable, use as STARTING PT 
+       for (i = 0; i < NPIXELS; i++) {
+           if (Pixel[i] == 0)
+               Serial.write ((byte)1);  // change 0s to 1s so not confused w/ STARTING PT
            else
-           {
-               natServo.write(straight);
-           }
+               Serial.write ((byte)Pixel[i]);  // writes bytes to serial port
+       }
+        
+          // find the position of the brightest pixel    ---- should change to better algorithm!!
+          for (i = 0; i < NPIXELS; i++) 
+          {
+            ch = Pixel[i];
+            if (ch < 0)     // takes care of any random negative values, if they somehow popup
+              ch += 256;
+            if (ch > maxpx) {   // Look for brightest pixel
+              maxi = i;
+              maxpx = ch;
+            }
+          }          
           
+//              if(  abs(history[memorySize-1] - maxi) < 15)    // try to account for sharp turns
+//              {                                               // ignore new brightest if change is too large
+//                for(i = 1; i < memorySize; i++){
+//                    history[i - 1] = history[i];
+//                }
+//                history[memorySize - 1] = maxi;
+//              }
+//              else
+//                maxi = history[memorySize - 1];
+              
+    ///// CONTROL STEERING ////////////////////////////////////////// 
+          if (maxi < 61) {
+              turnAngle = map(maxi, 0, 60, 44, 70);
+              natServo.write(turnAngle);
+          }
+          else if (maxi > 67) {
+              turnAngle = map(maxi, 68, 127, 72, 98);
+              natServo.write(turnAngle);
+          }
+          else {
+              natServo.write(straight);
+          }
+  
+  
     ///// CONTROL MOTOR SPEED/////////////////////////////////////////
-           motorPWM = 12;
-           motorSpeed = map(motorPWM, 0, 100, 0, 255);
-           
-           analogWrite(MOTORpin, motorSpeed);
-
+          motorPWM = 6;
+          motorSpeed = map(motorPWM, 0, 100, 0, 255);
+          analogWrite(MOTORpin, motorSpeed);
 }
+
